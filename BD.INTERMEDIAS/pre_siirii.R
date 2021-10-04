8df7db105ef4eda1f58c115bfcb13e091d1bc7a8
JoseMatta95 <josematta5511295@gmail.com>
2021-10-04 18:31:09 -0500

    04-10-21

--- BD.INTERMEDIAS/pre_siirii.R
+++ BD.INTERMEDIAS/pre_siirii.R
@@ -1,14 +1,63 @@
-data<-
+library(spdep)
+
+ data<-
   df3 %>% 
+  
   select(ANIO,ssirii) %>% 
   unnest(cols = c(ssirii)) %>%
-  #filter(vacunatipo%in%c("INFLU")) %>% 
+  #filter(ANIO == 2010 & vacunatipo == "PTV") %>%  
   
   left_join(region_shape, by = "DEPARTAMEN") %>% 
   
-  group_by(vacunatipo) %>% 
+  group_by(ANIO,vacunatipo) %>%
   
-  st_as_sf()
+  st_as_sf() %>% 
+  
+  nest() %>% 
+    
+    mutate(
+      nb= map(.x = data,
+               .f = ~poly2nb(.x, queen = T)),
+      
+      lw = map(.x = nb,
+               .f = ~nb2listw(.x, style = "W", zero.policy = T)),
+      
+      
+      moran = map2(.x = data, .y = lw,
+                     .f = ~moran.test(.x$sii,.y,alternative = "greater")),
+      
+      moran.mc = map2(.x = data, .y = lw,
+                      .f = ~moran.mc(.x$sii,.y, nsim = 9999, alternative = "greater")),
+      
+      
+      moran.t = map_dbl(.x = moran,
+                        .f = ~.x$estimate[[1]]),
+      
+      moran.p = map_dbl(.x = moran,
+                        .f = ~.x$p.value)
+    )
+ 
+data %>% 
+  select(ANIO,vacunatipo,moran.t,moran.p) %>% 
+  mutate(
+    alpha = ifelse(moran.p>0.05,0.2,1)
+  ) %>% 
 
-names(data)
-nb <- poly2nb(data, queen=TRUE)
+  ggplot(aes(x = as.factor(ANIO), y = vacunatipo, fill = moran.t, alpha = alpha))+
+  geom_tile()+
+  scale_x_discrete(position = "top") +
+  scale_fill_gradient(high = "#88A5B4")+
+  geom_text(aes(label = formatC(round(moran.t, 2), format = "f", digits = 2)))+
+  labs(x = "Years", y = NULL) +
+  guides(alpha = "none")+
+  theme_minimal()+
+  theme(
+    panel.grid = element_blank()
+  )
+  
+
+   
+  
+ 
+
+
