e52ac2d35b9e5d18cbdd9066cb263d060e6add0d
JoseMatta95 <josematta5511295@gmail.com>
2021-10-14 23:48:05 -0500

    14-10-21

--- BD.INTERMEDIAS/pre_siirii.R
+++ BD.INTERMEDIAS/pre_siirii.R
@@ -1,15 +1,13 @@
-prueba<-mov_general %>% filter(ANIO == 2020) %>% mutate(V005 = V005/1000000)
-
-test<-svydesign(id =~ V001, strata =~ V022, weights=weights(V005), data=prueba)
-
-svymean(~TIPORESIDENCIA, design = test, na.rm = T)
-
-waldo::compare(mov_general$V005,mov_general$V005_A)
-
-mov_general %>% 
-  select(V005) %>% 
+HOGAR1<-
+  HOGAR1 %>% 
   mutate(
-    t = V005/1000000
+    HV005_A = HV005/1000000000000
   )
 
+test <- svydesign(id =~ HV001, strata =~ HV022, weights=~HV005_A, data= HOGAR1)
 
+svymean(~HV025 %>% as.factor(), design = test, na.rm = T)
+
+HOGAR1$HV025 %>% class()
+
+table(HOGAR1$HV005_A)
