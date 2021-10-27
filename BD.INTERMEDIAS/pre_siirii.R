290c617d9336b1ed6fb4153f7aa3fb91b4520cdb
JoseMatta95 <josematta5511295@gmail.com>
2021-10-27 13:43:35 -0500

    27-10-2021

--- BD.INTERMEDIAS/pre_siirii.R
+++ BD.INTERMEDIAS/pre_siirii.R
@@ -1,42 +1,43 @@
-ggplot(data = df3$ssirii[[1]] %>%
-         left_join(region_shape) %>% 
-         st_as_sf() %>% 
-         #filter(ANIO%in%c(2010,2015,2019,2020)) %>% 
-         mutate(
-           siicat = ifelse(sii< -10,"< -10",
-                           ifelse(sii>=-10&sii<=-5,"-10 to -5",
-                                  ifelse(sii>=-5&sii<0,"-5 to 0",
-                                         ifelse(sii>=0&sii<5,"0 to 5",
-                                                ifelse(sii>=5&sii<10,"5 to 10",
-                                                       ifelse(sii>10,"> 10",NA)))))),
-           
-           
-           siicat2 = factor(siicat, levels = c("< -10","-10 to -5","-5 to 0",
-                                               "0 to 5","5 to 10","> 10"))
-           
-         )) +
-  geom_sf(alpha = 0.5, aes(fill=siicat2)) +
+t<-data %>% 
+  filter(ANIO!=2013) %>% 
+  mutate(
+    
+    ptv = map(.x =df,
+              .f = ~svyby(~MOV_PTV_e1, by=~INDICERIQUEZA + JUNTOS, design =.x, FUN=svytotal, na.rm = T) %>% 
+                
+                mutate(vacuna = MOV_PTV_e1,vacunatipo = "PTV",
+                       orden =as.numeric(as.factor(INDICERIQUEZA))) %>% 
+                
+                select(-MOV_PTV_e1) %>% 
+                
+                left_join(
+                  
+                  svyby(~INDICERIQUEZA, by=~JUNTOS, design = .x, FUN = svytotal, na.rm.all = T) %>%
+                    
+                    pivot_longer(cols = c(INDICERIQUEZA1ro:INDICERIQUEZA5to), names_to = "INDICERIQUEZA") %>% 
+                    group_by(JUNTOS,INDICERIQUEZA) %>% 
+                    slice(1) %>% 
+                    select(JUNTOS,INDICERIQUEZA,value)) %>%
+                
+                select(INDICERIQUEZA,JUNTOS,orden,vacuna,vacunatipo,se)))
 
-  scale_fill_manual(values = c("#531a23","#a63446","#e4c2c7","#b6cfde","#0c6291","#084465"))+
-  
-  guides(fill=guide_legend(
-    direction = "horizontal",
-    keyheight = unit(4, units = "mm"),
-    keywidth = unit(20/length(labels), units = "mm"),
-    title.position = 'top',
-    title.hjust = 0.5,
-    label.hjust = 1,
-    nrow = 1,
-    byrow = T,
-    label.position = "bottom"
-  ))+
-  
-  labs(fill = "Slope Index of Inequality") +
-  
-  theme(axis.text.x = element_text(angle = 30, size = 6),
-        strip.background = element_blank(),
-        panel.background = element_blank(),
-        strip.text = element_text(face = "bold"),
-        legend.position = "bottom",
-        legend.title = element_text(size = 9, face = "bold"),
-        legend.key.size = unit(0.5, 'cm'))
+t$ptv[[1]]
+
+data<-MOV_GENERAL %>% 
+  filter(JUNTOS == 1) %>% 
+  group_by(ANIO) %>% 
+  nest() %>%
+  mutate(
+    df= map(.x = data, 
+            .f = ~svydesign(id =~ V001, strata =~ V022, weights=~V005, data=.x))) 
+
+
+
+
+t<-
+  data %>% 
+  mutate(
+    pvt = map(.x = df,
+              .f = ~svyby(~INDICERIQUEZA, by=~JUNTOS, design = .x, FUN = svytotal, na.rm.all = T))
+  )
+
